5b1d6c95690fd9dfe3a3595af24cfb67858c6c33
Samith Wijesinghe <samithwijesinha@gmail.com>
2022-06-27 17:28:58 +0530

    [sc-150] - create the spritekit scene

--- gloopdrop/gloopdrop/GameViewController.swift
+++ gloopdrop/gloopdrop/GameViewController.swift
@@ -14,6 +14,26 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // create the view
+        if let view = self.view as! SKView? {
+        // create the scene
+            let scene = GameScene(size: view.bounds.size)
+        // set the scale mode to scale fill the view window
+            scene.scaleMode = .aspectFill
+        // set the background color
+            scene.backgroundColor = UIColor(red: 105/255,
+                                            green: 157/255,
+                                            blue: 181/255,
+                                            alpha: 1.0)
+        //present the scene
+            view.presentScene(scene)
+        // Set the view options
+            view.ignoresSiblingOrder = false
+            view.showsPhysics = false
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+        
     }
 
     override var shouldAutorotate: Bool {
