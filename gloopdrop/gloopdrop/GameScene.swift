0c1c0089a85b27be7dcf28a3d2dc421690f7e677
Samith Wijesinghe <samithwijesinha@gmail.com>
2022-06-28 16:19:04 +0530

    [sc-150] - setup the backgrounds and the anchor positions.

--- gloopdrop/gloopdrop/GameScene.swift
+++ gloopdrop/gloopdrop/GameScene.swift
@@ -11,7 +11,10 @@ import GameplayKit
 class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
-        
+    // set up the background
+        let background = SKSpriteNode(imageNamed: "background_1")
+        background.anchorPoint = CGPoint(x: 0, y: 0)
+        addChild(background)
     }
    
     }
